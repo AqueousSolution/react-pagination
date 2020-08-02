9060439029d751dc679d7668b31df05fb4ea9539
Idahosa Akioya <akioyaidahosa@yahoo.com>
2020-08-02 20:44:12 +0100

    Updates

---
+++ precache-manifest.e6ff4c8875bc33b91f00e64d66989916.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c6f30b131da1efe05a829d33b60b4db",
+    "url": "/react_pagination/index.html"
+  },
+  {
+    "revision": "7a3cf86fbaf0aa428b7b",
+    "url": "/react_pagination/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "25b42552fe5fe641106e",
+    "url": "/react_pagination/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7a3cf86fbaf0aa428b7b",
+    "url": "/react_pagination/static/js/2.7bd54b40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_pagination/static/js/2.7bd54b40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25b42552fe5fe641106e",
+    "url": "/react_pagination/static/js/main.4c54f690.chunk.js"
+  },
+  {
+    "revision": "3f54e12736931ec2cd68",
+    "url": "/react_pagination/static/js/runtime-main.603c1618.js"
+  }
+]);
